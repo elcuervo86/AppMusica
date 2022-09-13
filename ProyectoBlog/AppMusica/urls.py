62880cc7ba13dc466ca282f38745c192cf1eda50
Hernan Santamonica <hernan.santamonica@claro.com.ar>
2022-09-12 21:13:24 -0300

    Comienzo del proyecto

---
+++ ProyectoBlog/AppMusica/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from AppMusica import views
+
+urlpatterns = [
+   
+    path('', views.inicio, name="Inicio"), #esta era nuestra primer view
+    #path('vinilos', views.vinilos, name="Vinilos"),
+    #path('cds', views.cds, name="CDs"),
+    #path('clientes', views.clientes, name="Clientes"),
+    #path('leerCliente', views.leerClientes, name = "LeerClientes"),
+    #path('leerCd', views.leerCds, name = "LeerCds"),
+    #path('leerVinilo', views.leerVinilos, name = "LeerVinilos"),
+    #path('buscarCliente/', views.buscarCliente)
+
+]
